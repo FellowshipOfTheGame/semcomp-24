13726af2525832a83bf9ec7bffbb1183ce670fb0
Lucas Xavier Leite <lucasxavierleite@pm.me>
2021-07-15 20:20:48 -0300

    Adding bus stop (script + prefab)

---
+++ Assets/Scripts/BusStop/BusStop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusStop : MonoBehaviour
+{
+    [SerializeField]
+    private int healAmount;
+    
+    void Start() {
+        
+    }
+    
+    void Update() {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            other.gameObject.GetComponent<HealthSystem>().Heal(healAmount);
+        }
+    }
+}
