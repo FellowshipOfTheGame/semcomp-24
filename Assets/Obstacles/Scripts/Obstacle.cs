f629b205be0208d3db3567d34e6f0b9838fbf467
Lucas Xavier Leite <lucasxavierleite@pm.me>
2021-07-29 02:29:18 -0300

    Fixing/improving obstacle collision and damage system + test scene

---
+++ Assets/Obstacles/Scripts/Obstacle.cs
@@ -0,0 +1,134 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField]
+    private ObstaclePropertiesPreset obstaclePropertiesPreset;
+
+    private int baseDamage;
+    private int vehicleSpeedDecrease;
+    private float destroyCountdownPlayer;
+    private float destroyCountdownObstacle;
+
+    private float collisionTimeCount; // Seconds since collision with player or another obstacle
+    private bool hitPlayer;
+    private bool hitObstacle;
+
+    void Start()
+    {
+        baseDamage = obstaclePropertiesPreset.GetBaseDamage();
+        vehicleSpeedDecrease = obstaclePropertiesPreset.GetVehicleSpeedDecrease();
+        destroyCountdownPlayer = obstaclePropertiesPreset.GetDestroyCountdownPlayer();
+        destroyCountdownObstacle = obstaclePropertiesPreset.GetDestroyCountdownObstacle();
+    }
+
+    private void FixedUpdate()
+    {
+        if (hitPlayer || hitObstacle) {
+            DestroyCountdown();
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (!hitPlayer)
+        {
+            // Only the first hit deals damage and decreases player speed
+            if (other.gameObject.CompareTag("Player"))
+            {
+                GameObject player = other.gameObject;
+
+                // Simple damage calculation based only on the vehicle relative speed
+                float damage1 = other.relativeVelocity.magnitude;
+
+                // Damage calculation based on the dot product of the two vectors obtained from the collision
+
+                // Equation 1
+                float collisionDot1 = Vector3.Dot(other.relativeVelocity.normalized, other.GetContact(0).normal);
+                float damage2 = other.relativeVelocity.magnitude * Mathf.Abs(collisionDot1);
+
+                // Equation 2
+                float collisionDot2 = Vector3.Dot((player.transform.position - other.GetContact(0).point).normalized, other.GetContact(0).normal);
+                float damage3 = other.relativeVelocity.magnitude * Mathf.Abs(collisionDot2);
+
+                /*// DEBUG ---------------
+                
+                Debug.DrawRay(player.transform.position, other.GetContact(0).normal, Color.magenta);
+                Debug.DrawRay(player.transform.position, other.relativeVelocity + other.transform.rotation.eulerAngles, Color.cyan); // [equation 1]
+                Debug.DrawRay(player.transform.position, player.transform.position - other.GetContact(0).point, Color.green); // [equation 2]
+
+                Debug.Log("other.relativeVelocity: " + other.relativeVelocity);
+                Debug.Log("player.transform.position - other.GetContact(0).point: " + (player.transform.position - other.GetContact(0).point).normalized);
+
+                float angle1 = Vector3.Angle(other.relativeVelocity, other.GetContact(0).normal);
+                float angle2 = Vector3.Angle(player.transform.position - other.GetContact(0).point, other.GetContact(0).normal);
+                
+                Debug.Log("Angle between relativeVelocity and contact point normal (cyan/magenta) [equation 1]: " + angle1);
+                Debug.Log("Angle between 'that other vector' and contact point normal (green/magenta) [equation 2]: " + angle2);
+                
+                Debug.Log("Damage #1: " + damage1 + " (relative velocity)");
+                Debug.Log("Damage #2: " + damage2 + " (relative velocity * vector dot [equation 1]) : " + collisionDot1);
+                Debug.Log("Damage #3: " + damage3 + " (relative velocity * vector dot [equation 2] : " + collisionDot2);
+                
+                Time.timeScale = 0;
+                
+                // --------------------- */
+
+                int finalDamage = baseDamage + (int) Mathf.Floor(damage2 / 4f);
+                player.GetComponent<HealthSystem>().Damage(finalDamage);
+                Debug.Log("Damage: " + finalDamage);
+
+                // Decrease vehicle speed according to the damage dealt by the obstacle
+                other.rigidbody.AddRelativeForce(new Vector3(0, 0, -finalDamage * vehicleSpeedDecrease));
+
+                hitPlayer = true;
+
+                hitObstacle = false; // When a player hit happens after an obstacle hit, reset obstacle hit. The countdown time in effect is destroyCountdownPlayer
+                collisionTimeCount = 0f; // Reset time count. Discard time count since obstacle hit and start counting up to destroyCountdownPlayer
+            }
+            else if (other.gameObject.CompareTag("Obstacle"))
+            {
+                // When an obstacle hit happens after a player hit, the obstacle hit is ignored. The countdown time is effect is still destroyCountdownPlayer
+                hitObstacle = true;
+            }
+        }
+    }
+    
+    // If it's not possible to use rigidbody colliders due to performance issues, remove the component and use the collider as a trigger instead
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !hitPlayer)
+        {
+            GameObject player = other.gameObject;
+            
+            float damage = other.attachedRigidbody.velocity.magnitude;
+            int finalDamage = baseDamage + (int) damage / 10;
+            Debug.Log("Damage: " + finalDamage);
+            
+            player.GetComponent<HealthSystem>().Damage(finalDamage);
+
+            // Decrease vehicle speed according to the damage dealt by the obstacle
+            other.attachedRigidbody.AddRelativeForce(new Vector3(0, 0, -finalDamage * vehicleSpeedDecrease));
+
+            Destroy(gameObject);
+        }
+    }
+
+    // Destroy obstacle after countdown
+    private void DestroyCountdown()
+    {
+        collisionTimeCount += Time.deltaTime;
+
+        if ((hitPlayer && collisionTimeCount >= destroyCountdownPlayer) || (hitObstacle && collisionTimeCount >= destroyCountdownObstacle))
+        {
+            if (hitPlayer)
+            {
+                Debug.Log("Destroying obstacle after collision with player (" + collisionTimeCount + " s)");
+            } else {
+                Debug.Log("Destroying obstacle after collision with another obstacle (" + collisionTimeCount + " s)");
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/BusTest.cs
+++ Assets/Scripts/BusTest.cs
@@ -1,37 +1,25 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class BusTest: MonoBehaviour
 {
     private Rigidbody _rigidbody;
 
-    public InputAction movement;
-
-    [SerializeField]
-    private int maxSpeed;
-
     [SerializeField]
     private TextMeshProUGUI currentSpeedText;
     
-    void Start() {
+    void Start()
+    {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate() {
-        _rigidbody.AddForce(new Vector3(movement.ReadValue<Vector2>().x, 0, movement.ReadValue<Vector2>().y) * (maxSpeed * Time.fixedDeltaTime), ForceMode.VelocityChange);
-        _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, maxSpeed);
+    void FixedUpdate()
+    {
         currentSpeedText.text = (_rigidbody.velocity.magnitude).ToString("00.00000");
     }
-
-    private void OnEnable() {
-        movement.Enable();
-    }
     
-    private void OnDisable() {
-        movement.Disable();
+    public void Resume()
+    {
+        Time.timeScale = 1f;
     }
 }
