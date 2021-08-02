3a492a19a362695ef5910d30e7ef59b5e309d597
Julia <juliafrare7@gmail.com>
2021-08-02 16:37:48 -0300

    adds shop buy checks and operations

--- Server/src/controllers/shopController.js
+++ Server/src/controllers/shopController.js
@@ -1,8 +1,9 @@
 // Dependencies
 const Redis = require("ioredis");
-
-
 const redis = new Redis({ port: config.REDIS_PORT, host: config.REDIS_HOST}); 
+
+const UserModel = require("../models/User")
+const ShopModel = require("../models/Shop")
 
 // Exporting controller async functions
 module.exports = { 
@@ -15,10 +16,15 @@ module.exports = {
 //shop function: retrieves items from shop, retrieves and displays the amount of money user has
 //GET request
 async function shop(req, res) {
-    const userId = "user1"  //string
-    const userMoney = req.body?.money   //integer
+    const userId = "60f32e0105e0c858b8746d75"  //string
+    const userGold = parseInt(req.body?.gold)   //integer
     const userItems = req.body?.items   //list
-    const items = { "thing 1": 100, "thing 2": 200, "thing 3": 300 }  //dict of all items on shop and their "prices"
+
+    const shopItems = {
+        "thing 1": 100,
+        "thing 2": 200,
+        "thing 3": 300
+    }  //dict of all items on shop and their "prices"
 
     //TODO: if user already has an item, do not display it / have it "unavailable"
     //else display it as usual
@@ -29,16 +35,30 @@ async function shop(req, res) {
 //buy function: exchanges user's money for the item they want to buy
 //POST request
 async function buy(req, res) { 
-
-    const userId = "user1"  //string
-    const userMoney = req.body?.money   //integer
+    const userId = "60f32e0105e0c858b8746d75"  //string
+    const userGold = parseInt(req.body?.gold)   //integer
     const userItems = req.body?.items   //list
-    const target_item = req.body?.item.toString()   //string
+    const targetItem = req.body?.item.toString()   //string
 
-    //TODO: check if user has already purchased the item
-    //TODO: check if user has enough money for the transaction
-    //if both are true, subtract user's money and add item to his "acquired items"
-    //else cancel transaction
+    const shopItems = {
+        "thing 1": 100,
+        "thing 2": 200,
+        "thing 3": 300
+    }  //dict of all items on shop and their "prices"
 
-    return res.json({ serverStatus: "OK", endpoint: "shop buy" })
+    //check if user has already purchased the item
+    if(targetItem in userItems)
+        return res.status(400).json({ message: "User already purchased this item!" })
+    
+    //check if user has enough gold for the transaction
+    if(shopItems[targetItem] > userGold)
+        return res.status(400).json({ message: "User doesn't have enough gold!" })
+
+    //if both are true, subtract user's gold and add item to his "acquired items"
+    userGold = userGold - shopItems[targetItem]
+    userItems.push(targetItem)
+
+    //TODO: add change to database
+
+    return res.json({ message: "ok" })
 }
