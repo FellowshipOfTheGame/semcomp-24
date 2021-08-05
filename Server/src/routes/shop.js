1a58a5a2aed0d10fcc4b041335606c2d8414a499
Julia <juliafrare7@gmail.com>
2021-08-04 23:19:55 -0300

    adds authentication

--- Server/src/routes/shop.js
+++ Server/src/routes/shop.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const routes = express.Router();
 
+// Middlewares
+const SessionMiddleware = require('../middlewares/Session.middleware');
+
 // Controllers
 const shopController = require('../controllers/shopController')
 
 // Routes
-routes.get('/', shopController.shop)
-routes.post('/buy-upgrades', shopController.buy)
+routes.get('/', SessionMiddleware.isAuth, shopController.shop)
+routes.post('/buy-upgrades', SessionMiddleware.isAuth, shopController.buy)
 
 // Export routes
 module.exports = routes;
