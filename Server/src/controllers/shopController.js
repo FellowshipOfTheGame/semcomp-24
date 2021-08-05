a975260981aaeff187de86f92a88eaf828779711
Julia <juliafrare7@gmail.com>
2021-08-04 22:49:37 -0300

    buy function updates database

--- Server/src/controllers/shopController.js
+++ Server/src/controllers/shopController.js
@@ -11,20 +11,18 @@ module.exports = {
 
 // Controller Functions
 
-//create shop display for user based on his acquired upgrades
+// create shop display for user based on his acquired upgrades
 async function createShop(userUpgrades) {
     const names = ["Max Life", "Base Acceleration", "Traction", "Booster", "Nitro", "Bus Stop"]
     var shopUpgrades = []
     
     for(let i = 0; i < names.length; i++) {
-        const userLevel = userUpgrades[i].level
-
-        console.log(userLevel)
+        const upLevel = userUpgrades[i].level + 1
 
         item = await ShopModel.create({
             itemName: names[i],
-            level: userLevel,
-            price: (userLevel + 1) * 100,
+            level: upLevel,
+            price: upLevel * 100,
             created_at: new Date(),
             updated_at: new Date(),
         });
@@ -35,24 +33,25 @@ async function createShop(userUpgrades) {
     return shopUpgrades
 }
 
-//shop function: retrieves upgrades from shop, retrieves and displays the amount of gold user has
-//GET request
+// shop function: retrieves upgrades from shop, retrieves and displays the amount of gold user has
+// GET request
 async function shop(req, res) {
     const userId = req.query?.user_id  //string
 
-    const userGold = await UserModel.findById(userId).select("gold");
-    const userUpgrades = await UserModel.findById(userId).select("upgrades");
+    var user = await UserModel.findById(userId)
+    var userGold = user.gold
+    var userUpgrades = await user.upgrades
 
     // Shop model
     const shopUpgrades = await createShop(userUpgrades)
 
-    //TODO: if user already has an upgrade, do not display it / have it "unavailable"
-    //else display it as usual
+    // TODO: if user already has an upgrade, do not display it / have it "unavailable"
+    // else display it as usual
     return res.status(200).json({ message: "ok", gold: userGold, shop: shopUpgrades })
 }
 
-//buy function: exchanges user's gold for the upgrade they want to buy
-//POST request
+// buy function: exchanges user's gold for the upgrade they want to buy
+// POST request
 async function buy(req, res) { 
     const userId = req.query?.user_id  //string
 
@@ -61,22 +60,26 @@ async function buy(req, res) {
     const upgradeLevel = req.body?.upgrade_level  //integer
     const upgradePrice = req.body?.upgrade_price  //integer
 
-    var userGold = await UserModel.findById(userId).select("gold");
-    var userUpgrades = await UserModel.findById(userId).select("upgrades");
+    var user = await UserModel.findById(userId)
+    var userGold = user.gold
+    var userUpgrades = await user.upgrades
 
-    //check if user has already purchased the upgrade
+    // check if user has already purchased the upgrade
     if(upgradeLevel !== userUpgrades[upgradeIndex].level + 1)
         return res.status(400).json({ message: "User cannot purchase this upgrade!" })
     
-    //check if user has enough gold for the transaction
+    // check if user has enough gold for the transaction
     if(upgradePrice > userGold)
         return res.status(400).json({ message: "User doesn't have enough gold!" })
 
-    //if both are true, subtract user's gold and add upgrade to his "acquired upgrades"
+    // if both are true, subtract user's gold and add upgrade to his "acquired upgrades"
     userGold -= upgradePrice
     userUpgrades[upgradeIndex].level++
 
-    //TODO: add change to database
+    // add change to database
+    user.gold = userGold
+    user.upgrades[upgradeIndex].level = userUpgrades[upgradeIndex].level
+    await user.save()
 
     return res.status(200).json({ message: "ok", userGold: userGold, upgrade: upgradeName, upgrade_level: upgradeLevel })
 }
