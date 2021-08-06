1086908927ae8a026d347da5b15376cbe88ac238
Van Loon <gabriel.vl.rojas@gmail.com>
2021-08-06 17:38:17 -0300

    update: create Session and Mongoose loaders

--- Server/src/config/index.js
+++ Server/src/config/index.js
@@ -4,11 +4,10 @@ dotenv.config();
 
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || "dev",
-    ENABLE_HTTP: process.env.ENABLE_HTTP == "1" || false,
-    ENABLE_HTTPS: process.env.ENABLE_HTTP == "1" || false,
+    ENABLE_HTTPS: process.env.ENABLE_HTTPS == "1" || false,
     
-    SERVER_HTTP_PORT: process.env.SERVER_HTTP_PORT || 3000,
-    SERVER_HTTPS_PORT: process.env.SERVER_HTTPS_PORT || undefined,
+    SERVER_TRUST_PROXY: process.env.SERVER_TRUST_PROXY == "1" || false,
+    SERVER_PORT: process.env.SERVER_PORT || 3000,
     
     CERTIFICATE_KEY_PATH: process.env.CERTIFICATE_KEY_PATH || undefined,
     CERTIFICATE_CERT_PATH: process.env.CERTIFICATE_CERT_PATH || undefined,
@@ -17,10 +16,16 @@ module.exports = {
     REDIS_HOST: process.env.REDIS_HOST || "localhost",
     REDIS_PORT: process.env.REDIS_PORT ||  6379,
 
-    MONGO_CONNECT_URL: process.env.REDIS_HOST.MONGO_CONNECT_URL || "mongodb://localhost:27017/myapp",
+    MONGO_CONNECT_URL: process.env.MONGO_CONNECT_URL || "mongodb://localhost:27017/myapp",
 
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || undefined,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET|| undefined,
 
+    COOKIE_SIGNATURE_KEY: process.env.COOKIE_SIGNATURE_KEY || "MINHASENHAMEGASECRETA",
+    SESSION_SECURE: process.env.SESSION_SECURE == "1" || false, 
+    SESSION_HTTP_ONLY: process.env.SESSION_HTTP_ONLY == "1" || false,
+    SESSION_SAME_SITE: process.env.SESSION_SAME_SITE || "strict",
+    SESSION_MAX_AGE: process.env.SESSION_MAX_AGE || 3600000,
+    
     REQUEST_SIGNATURE_KEY: process.env.REQUEST_SIGNATURE_KEY || "MINAHSENHASUPERSECRETA",
 }
