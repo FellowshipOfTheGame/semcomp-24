964c5e23ef90bf519d70dff111affba2673a60a3
Van Loon <gabriel.vl.rojas@gmail.com>
2021-08-16 15:34:25 -0300

    update: minor fixes in shop controller

--- Server/src/controllers/shopController.js
+++ Server/src/controllers/shopController.js
@@ -18,10 +18,11 @@ async function shop(req, res) {
     const userUpgrades = req.user.upgrades
 
     // makes copy to avoid referencing the original
-    let shopUpgrades = JSON.parse(JSON.stringify(userUpgrades))
+    // let shopUpgrades = JSON.parse(JSON.stringify(userUpgrades))
+    let shopUpgrades = Object.assign([], userUpgrades);
 
     if (!shopUpgrades) {
-        return res.status(404).json({ message: "Upgrades not found" })
+        return res.status(404).json({ message: "upgrades not found" })
     }
 
     // if user already has an upgrade maximized, do not display it
@@ -29,6 +30,7 @@ async function shop(req, res) {
         obj.level++ // add +1 to level
         return (obj.level < 4);
     })
+    .map((obj) => {return { itemName: obj.itemName, level: obj.level, price: obj.price }})
 
     // else display it as usual
     return res.status(200).json({ message: "ok", gold: userGold, shop: shopUpgrades })
@@ -39,7 +41,7 @@ async function shop(req, res) {
 async function buy(req, res) { 
 
     const userId = req.user._id  //string
-    const upgradeName = req.body?.upgrade_name.toString()   //string
+    const upgradeName = req.body?.itemName.toString()   //string
     let userGold = req.user.gold
     let userUpgrades = req.user.upgrades
 
@@ -47,7 +49,7 @@ async function buy(req, res) {
     let currUpgrade = userUpgrades.find(obj => obj.itemName === upgradeName)
 
     if(!currUpgrade)
-        return res.status(404).json({ message: "Upgrade not found" })
+        return res.status(404).json({ message: "upgrade not found" })
 
     // target upgrade values
     // makes copy to avoid referencing the original
@@ -61,11 +63,11 @@ async function buy(req, res) {
 
     // check if user has already maximized the upgrade 
     if(upgradeLevel > 3)
-        return res.status(400).json({ message: "User already maximized this upgrade!" })
+        return res.status(400).json({ message: "user already maximized this upgrade" })
     
     // check if user has enough gold for the transaction
     if(upgradePrice > userGold)
-        return res.status(400).json({ message: "User doesn't have enough gold!" })
+        return res.status(400).json({ message: "user doesn't have enough gold" })
 
     // if both are true, subtract user's gold and add upgrade to his "acquired upgrades"
     userGold -= upgradePrice
@@ -77,11 +79,12 @@ async function buy(req, res) {
 
     try {
         await req.user.save()
+        .then(() => { 
+            return res.status(200).json({ message: "ok", gold: userGold, itemName: upgradeName, level: upgradeLevel, price: upgradePrice })
+        })
     }
     catch (err) {
-        console.log(`Failed to save upgrade ${upgradeName} to user ${userId}`)
-        res.status(500).json({ message: "Internal server error" });
+        console.error(`Failed to save upgrade ${upgradeName} to user ${userId}. ${err}`)
+        return res.status(500).json({ message: "Internal server error" });
     }
-
-    return res.status(200).json({ message: "ok", userGold: userGold, upgrade: upgradeName, upgrade_level: upgradeLevel, upgrade_price: upgradePrice })
 }
