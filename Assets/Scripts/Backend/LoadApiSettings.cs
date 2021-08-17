558f9dd87519812d6509c6f0f943850568dfd5fa
Tiago Marino <tmsilva11@gmail.com>
2021-08-17 16:12:58 -0300

    Refactor Race Requests

--- Assets/Scripts/Backend/LoadApiSettings.cs
+++ Assets/Scripts/Backend/LoadApiSettings.cs
@@ -1,23 +1,25 @@
 ﻿using UnityEngine;
-using System.IO;
+using SubiNoOnibus.Networking;
 
 public class LoadApiSettings : MonoBehaviour
-{
+{   
     private const string _apiSettingsPath = "Secrets/api_settings";
-
-    [SerializeField] private TMPro.TextMeshProUGUI textMesh;
-
     private void Awake()
+    {
+        LoadSettings();
+    }
+
+    private void LoadSettings()
     {
         var json = Resources.Load<TextAsset>(_apiSettingsPath);
         var settings = JsonUtility.FromJson<ApiSettings>(json.text);
 
         Endpoints.Base_url = settings.api_url;
-        textMesh.SetText(settings.api_url);
+        Cryptography.request_signature_key = settings.signature_key;
     }
 
     [System.Serializable]
-    private struct ApiSettings
+    public struct ApiSettings
     {
         public string api_url;
         public string signature_key;
--- Assets/Scripts/RaceStartTest.cs
+++ Assets/Scripts/RaceStartTest.cs
@@ -1,71 +1,30 @@
-using System;
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Networking;
-using System.Security.Cryptography;
-using System.Text;
+using SubiNoOnibus.Networking.Requests;
 
-public partial class RaceStartTest : MonoBehaviour
+public class RaceStartTest : MonoBehaviour
 {
     [SerializeField] private RaceData raceData;
-
-    private IEnumerator StartRace()
-    {
-        using UnityWebRequest request = WebRequestFactory.AuthPostJson(Endpoints.Race_start_url, "{}");
-        yield return request.SendWebRequest();
-
-        if (request.result != UnityWebRequest.Result.Success)
-        {
-            Debug.Log(request.error);
-        }
-        else
-        {
-            raceData = JsonUtility.FromJson<RaceData>(request.downloadHandler.text);
-            Debug.Log(request.result);
-        }
-        Debug.Log(request.downloadHandler.text);
-    }
 
     [ContextMenu("FinishRace")]
     public void StartCoroutineFinishRace()
     {
-        raceData.sign = string.Empty;
-
-        //TODO: ler chave do api_settings.json
-        string signature = string.Empty;
-        byte[] unicodeKey = Encoding.UTF8.GetBytes("MINAHSENHASUPERSECRETA");
-        using (HMACSHA256 hmacSha256 = new HMACSHA256(unicodeKey))
-        {
-            string signString = JsonUtility.ToJson(raceData);
-            Debug.Log(signString);
-            Byte[] dataToHmac = Encoding.UTF8.GetBytes(signString);
-            signature = Convert.ToBase64String(hmacSha256.ComputeHash(dataToHmac));
-        }
-
-        raceData.sign = signature;
-
-        StartCoroutine(FinishRace());
+        var finishRaceEnumerator = RaceRequestHandler.FinishRace(raceData, 
+            () => Debug.Log("Success"), 
+            (req) =>
+            {
+                Debug.Log(req.responseCode);
+            }
+        );
+        StartCoroutine(finishRaceEnumerator);
     }
 
     [ContextMenu("StartRace")]
     public void StartCoroutineStartRace()
     {
-        StartCoroutine(StartRace());
-    }
-
-    private IEnumerator FinishRace()
-    {
-        using UnityWebRequest request = WebRequestFactory.AuthPostJson(Endpoints.Race_finish_url, JsonUtility.ToJson(raceData));
-        yield return request.SendWebRequest();
-
-        if (request.result != UnityWebRequest.Result.Success)
-        {
-            Debug.Log(request.error);
-        }
-        else
-        {
-            Debug.Log(request.result);
-        }
-        Debug.Log(request.downloadHandler.text);
+        var startRaceEnumerator = RaceRequestHandler.StartRace(
+            (raceData) => Debug.Log(raceData),
+            (req) => Debug.Log(req.error)
+        );
+        StartCoroutine(startRaceEnumerator);
     }
 }
