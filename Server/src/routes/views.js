f39813bf20ec0f888255da9498dbda10c57f3d3a
Van Loon <gabriel.vl.rojas@gmail.com>
2021-09-07 18:04:37 -0300

    add: Login & Error Message

--- Server/src/routes/views.js
+++ Server/src/routes/views.js
@@ -3,18 +3,37 @@ const routes = require('express').Router();
 const ejs    = require('ejs')
 const path   = require('path')
 
+const config = require('../config')
+
 const templates = path.join(__dirname, '../templates')
+const publicUrl = `${config.SERVER_PATH_PREFIX}`
+
 
 // Views Routes
 routes.get('/', async (req, res) => { 
     ejs.renderFile(`${templates}/index.html`, 
         {
-            title: 'Home'
+            title: 'Home',
+            main:  'home.html',
+            public: publicUrl,
         },
         function(err, html) { 
         return res.send(html)
     })
 })
+
+routes.get('/informacoes', async (req, res) => { 
+    ejs.renderFile(`${templates}/index.html`, 
+        {
+            title: 'Políticas & Termos',
+            main:  'about.html',
+            public: publicUrl,
+        },
+        function(err, html) { 
+        return res.send(html)
+    })
+})
+
 
 
 
