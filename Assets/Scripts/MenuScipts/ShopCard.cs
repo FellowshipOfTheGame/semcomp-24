785ad40712f88d1680b2884a52aaaa0c38cc5932
Tiago Marino <tmsilva11@gmail.com>
2021-09-10 22:11:54 -0300

    Add buy-upgrades request

---
+++ Assets/Scripts/MenuScipts/ShopCard.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using SubiNoOnibus.UI;
+using UnityEngine.Events;
+
+public class ShopCard : MonoBehaviour
+{
+    [SerializeField] private SpriteArraySO spriteArray;
+    
+    [SerializeField] private Image background;
+    [SerializeField] private Button buyButton;
+    [SerializeField] private TMPro.TextMeshProUGUI itemValueTxt;
+    
+    private ShopItem _item;
+    private ShopMenu _shopMenu;
+
+    public void SetShopMenu(ShopMenu shopMenu)
+    {
+        _shopMenu = shopMenu;
+        buyButton.onClick.AddListener(BuyItem);
+    }
+
+    public void SetShopItem(ShopItem item)
+    {
+        _item = item;
+
+        itemValueTxt.SetText(item.price.ToString());
+
+        int index = item.level - 1;
+        try
+        {
+            background.sprite = spriteArray.sprites[index];
+        }
+        catch
+        {
+            Debug.LogError("Correspondent sprite for item level not found");
+        }
+    }
+
+    private void BuyItem()
+    {
+        _shopMenu.TryBuyItem(_item);
+    }
+}
--- Assets/Scripts/MenuScipts/ShopMenu.cs
+++ Assets/Scripts/MenuScipts/ShopMenu.cs
@@ -1,67 +1,122 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
+using SubiNoOnibus.Networking.Requests;
+using System.Collections.Generic;
 
 namespace SubiNoOnibus.UI
 {
-    public class ShopMenu : MonoBehaviour
+    public class ShopMenu : MonoBehaviour, IMenu
     {
+        public int Gold { get => _gold;  private set => UpdateGoldAmount(value); }
+        
         [SerializeField] private PopupMessageWindow logInterface;
+        [SerializeField] private Transform cardsContainer;
+        [SerializeField] private TMPro.TextMeshProUGUI goldAmountTxt;
 
-        private ShopUpgrades shopUpgrades;
-        private int gold;
+        private ShopUpgrades _shopUpgrades;
+        private int _gold = 0;
+        private Dictionary<string, ShopCard> _shopCards;
 
-        private void OnEnable()
+        public void Open()
         {
-            StartCoroutine(GetShopUpgrades());
+            IEnumerator getShopUpgrades = ShopRequestHandler.GetShopUpgrades
+            (
+                HandleGetShopUpgradesSuccess, 
+                HandleError
+            );
+            StartCoroutine(getShopUpgrades);
         }
 
-        private IEnumerator GetShopUpgrades()
+        public void Close()
         {
-            using UnityWebRequest request = WebRequestFactory.AuthGet(Endpoints.Shop_url);
-
-            yield return request.SendWebRequest();
-
-            if (request.result != UnityWebRequest.Result.Success)
-            {
-                Debug.Log(request.error);
-                HandleGetShopUpgradesError(request);
-            }
-            else
-            {
-                Debug.Log(request.result);
-                Debug.Log(request.downloadHandler.text);
-
-                shopUpgrades = JsonUtility.FromJson<ShopUpgrades>(request.downloadHandler.text);
-                PopulateShopList();
-            }
-            Debug.Log(request.downloadHandler.text);
+            EnableVisuals(false);
         }
 
-        private void HandleGetShopUpgradesError(UnityWebRequest request)
+        public void TryBuyItem(ShopItem item)
+        {
+            IEnumerator buyShopUpgrade = ShopRequestHandler.BuyShopUpgrade
+            (
+                item,
+                HandleBuyUpgradeSuccess,
+                HandleError
+            );
+            StartCoroutine(buyShopUpgrade);
+        }
+
+        private void EnableVisuals(bool value)
+        {
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(value);
+            }
+        }
+        private void HandleBuyUpgradeSuccess(NewShopItem newItem)
+        {
+            Gold = newItem.gold;
+
+            if(_shopCards.TryGetValue(newItem.itemName, out ShopCard card))
+            {
+                card.SetShopItem(newItem);
+            }
+        }
+
+        private void HandleGetShopUpgradesSuccess(ShopUpgrades shopUpgrades)
+        {
+            _shopUpgrades = shopUpgrades;
+            PopulateShopList();
+            EnableVisuals(true);
+        }
+
+        private void HandleError(UnityWebRequest request)
         {
             string errorMsg = JsonUtility.FromJson<ErrorMessageData>(request.downloadHandler.text);
-
-            if (request.responseCode == 401)
-            {
-
-            }
-            else if (request.responseCode == 404)
-            {
-
-            }
-
             logInterface.LogError(errorMsg);
         }
 
         private void PopulateShopList()
         {
-            gold = shopUpgrades.gold;
-            Debug.Log("gold: " + gold);
-            foreach (var item in shopUpgrades.shop)
+            Gold = _shopUpgrades.gold;
+            
+            
+            foreach (ShopItem item in _shopUpgrades.shop)
             {
-                Debug.Log($"{item.itemName} ({item.level}): {item.price}");
+                if (_shopCards.TryGetValue(item.itemName, out ShopCard card))
+                {
+                    card.SetShopItem(item);
+                }
+                else
+                {
+                    Debug.LogWarning("Incoming item with no correspondent ShopCard: " + item);
+                }
+            }
+
+            EnableVisuals(true);
+        }
+
+        private void UpdateGoldAmount(int amount)
+        {
+            _gold = amount;
+            goldAmountTxt.SetText(_gold.ToString());
+        }
+
+        private void Awake()
+        {
+            PopulateShopCardDictionary();
+        }
+
+        private void PopulateShopCardDictionary()
+        {
+            _shopCards = new Dictionary<string, ShopCard>(cardsContainer.childCount);
+            
+            foreach(Transform card in cardsContainer)
+            {
+                ShopCard shopCard = card.GetComponent<ShopCard>();
+                
+                shopCard.SetShopMenu(this);
+                _shopCards[card.name] = shopCard;
             }
         }
+
     }
 }
