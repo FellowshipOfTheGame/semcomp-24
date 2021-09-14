8a77b4048bc972f19767375139bd74357b0eaf2d
Lucas Xavier Leite <lucasxavierleite@pm.me>
2021-09-14 20:55:40 -0300

    Add scenery generation

--- Assets/LevelGeneration/ObjectSet.cs
+++ Assets/LevelGeneration/ObjectSet.cs
@@ -6,6 +6,8 @@ public class ObjectSet : ScriptableObject
 {
     [SerializeField] private List<GameObject> members;
 
+    public List<GameObject> Members => members;
+
     public GameObject GetRandom()
     {
         int randomIndex = Random.Range(0, members.Count);
