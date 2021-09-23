54b46efc1d7b65247082864d46df04d5eda58dd3
Tiago Marino <tmsilva11@gmail.com>
2021-09-23 00:15:09 -0300

    Add random phrases while loading audio bank, remove old menu animations

--- Assets/Scripts/Utils/LoadFModOnInit.cs
+++ Assets/Scripts/Utils/LoadFModOnInit.cs
@@ -4,10 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class LoadFModOnInit : MonoBehaviour
 {
+    [SerializeField] private string[] phrases;
+    [SerializeField] private TMPro.TextMeshProUGUI textMesh;
+
+    private void Awake()
+    {
+        string phrase = phrases[Random.Range(0, phrases.Length)];
+        textMesh.SetText(phrase);
+    }
+
     public IEnumerator Start()
     {
-        yield return new WaitUntil(() => FMODUnity.RuntimeManager.HasBankLoaded("Master"));
-        
+        yield return new WaitForSeconds(2f);
+        yield return new WaitUntil(() => FMODUnity.RuntimeManager.HasBanksLoaded);
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
 }
