359a38147416b5d2136634c255863f1adee3a548
Tiago Marino <tmsilva11@gmail.com>
2021-09-23 03:21:33 -0300

    Instructions menu

---
+++ Assets/Scripts/MenuScripts/InstructionsCard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace SubiNoOnibus.UI
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Instructions")]
+    public class InstructionsCard : ScriptableObject
+    {
+        public InstructionCard[] instructions;
+
+        public bool TryGetAt(int index, out InstructionCard? card)
+        {
+            if (index >= 0 && index < instructions.Length)
+            {
+                card = instructions[index];
+                return true;
+            }
+            else
+            {
+                card = null;
+                return false;
+            }
+        }
+    }
+
+    [System.Serializable]
+    public struct InstructionCard
+    {
+        public string text;
+        public Sprite sprite;
+    }
+}
